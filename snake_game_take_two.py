92b1fd7ea894e15c25e09aa8d5ff0e39defcf56c
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-02-09 22:58:29 +0200

    Added a snake and food display

--- snake_game_take_two.py
+++ snake_game_take_two.py
@@ -4,7 +4,7 @@ import pygame
 pygame.init()
 
 # Set up the game window
-window_size = (400, 400)
+window_size = (800, 600)
 window = pygame.display.set_mode(window_size)
 # pygame.display.set_caption("Snake Game")
 
@@ -20,8 +20,25 @@ while running:
             running = False
     
     # Clear the screen
-    window.fill(black)
+    window.fill(black) # background colour
     
+    # Handle users input
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_UP]:
+    #     # Move the snake up
+    # elif keys[pygame.K_DOWN]:
+    #     # Move the snake down
+    # elif keys[pygame.K_LEFT]:
+    #     # Move the snake left
+    # elif keys[pygame.K_RIGHT]:
+    #     # Move the snake right
+
+    # Draw the snake
+    pygame.draw.rect(window, (0, 255, 0), (100, 100, 50, 50))
+
+    # Draw the food
+    pygame.draw.rect(window, (255, 0, 0), (200, 200, 25, 25))
+
     # Update the display
     pygame.display.update()
 
