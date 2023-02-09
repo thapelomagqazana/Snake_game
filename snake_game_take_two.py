4bbf32bdb4c1d49a0f221d59074bf23e8de50605
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-02-09 23:43:01 +0200

    Added snake response to user input and detect game over condition as well as created snake and food item

--- snake_game_take_two.py
+++ snake_game_take_two.py
@@ -4,13 +4,28 @@ import pygame
 pygame.init()
 
 # Set up the game window
-window_size = (800, 600)
+window_x = 800
+window_y = 600
+window_size = (window_x, window_y)
 window = pygame.display.set_mode(window_size)
 # pygame.display.set_caption("Snake Game")
 
 # Define colors
 white = (255, 255, 255)
 black = (0, 0, 0)
+green = (0, 255, 0)
+red = (255, 0, 0)
+
+# Define the initial position of the snake.
+snake_x = window.get_width() // 2 - 25
+snake_y = window.get_height() // 2 - 25
+
+# Define the size of the snake.
+snake_width = 50
+snake_height = 50
+
+# Define the speed of the snake
+snake_speed = 5
 
 # Run the game loop
 running = True
@@ -22,22 +37,41 @@ while running:
     # Clear the screen
     window.fill(black) # background colour
     
+    # Get the state of all the currently pressed keys
+    keys = pygame.key.get_pressed()
+
     # Handle users input
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_UP]:
-    #     # Move the snake up
-    # elif keys[pygame.K_DOWN]:
-    #     # Move the snake down
-    # elif keys[pygame.K_LEFT]:
-    #     # Move the snake left
-    # elif keys[pygame.K_RIGHT]:
-    #     # Move the snake right
+    if keys[pygame.K_UP]:
+        # Move the snake up
+        snake_y -= snake_speed
+    elif keys[pygame.K_DOWN]:
+        # Move the snake down
+        snake_y += snake_speed
+    elif keys[pygame.K_LEFT]:
+        # Move the snake left
+        snake_x -= snake_speed
+    elif keys[pygame.K_RIGHT]:
+        # Move the snake right
+        snake_x += snake_speed
+
+    # Update the game state
+
+    # Check if the game is over
+    if snake_x < 0 or snake_x > window_x or snake_y < 0 or snake_x > window_y:
+        # Display the game over screen
+        font = pygame.font.Font(None, 36)
+        game_over_text = font.render('Game Over!', True, red)
+        window.blit(game_over_text, (350, 300))
+        pygame.display.update()
+        running = False
 
     # Draw the snake
-    pygame.draw.rect(window, (0, 255, 0), (100, 100, 50, 50))
+    pygame.draw.rect(window, green, (snake_x, snake_y, snake_width, snake_height))
 
     # Draw the food
-    pygame.draw.rect(window, (255, 0, 0), (200, 200, 25, 25))
+    pygame.draw.rect(window, red, (200, 200, 25, 25))
+
+
 
     # Update the display
     pygame.display.update()
